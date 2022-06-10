63ea952d19fcf74ff8736b98b567f13e3a96a590
Finn818 <jsolomons239@gmail.com>
2022-06-10 14:08:09 +0200

    adding

---
+++ javascript_workshop/dynamicElement/code.js
@@ -0,0 +1,19 @@
+// function smartPeople() {
+//     document.getElementById()
+//     console.log(smartPeople);
+// }
+
+ let smartPeople = ['Joshua',' Ridhaa', 'Cameron', 'Cassidy', 'Emile'];
+console.log(smartPeople)
+smartPeople.forEach((smartPeople, i)=>{
+});
+
+document.write("Joshua, Ridhaa, Cameron, Cassidy, Emile");
+
+    // let smartPeoples = (smartPeople)=> {
+    //     document.write(` {ul}`);
+    // }
+    
+    // function ul() {
+        
+      
